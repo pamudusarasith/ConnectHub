38400a0f36c003c41da784ce2f05396060263660
Sachinthaka Gayashan <sachinthakagayashan44@gmail.com>
2024-08-30 16:36:50 +0530

    Create API endpoints for posts

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,10 @@
 import express from "express";
 import auth from "./auth.js";
+import post from "./post.js";
 
 const router = express.Router();
 
 router.use("/", auth);
+router.use("/post", post);
 
 export default router;
