2b2d2d1e46dd7dd070321e1398831caf195749b6
Sachinthaka Gayashan <sachinthakagayashan44@gmail.com>
2024-09-01 20:19:31 +0530

    adding a menu button to update or delete post

--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -7,43 +7,65 @@ import {
   Avatar,
   IconButton,
   Typography,
+  Menu,
+  MenuItem,
 } from "@mui/material";
-import { ThumbUp, Comment } from "@mui/icons-material";
+import { ThumbUp, Comment,MoreVert } from "@mui/icons-material";
 
+function Post(post) {
+  const [anchorE1, setAnchorE1] = useState(null);
 
-<Card sx={{ maxWidth: "100%" }}>
-<CardHeader
-  avatar={
-    <Avatar aria-label="recipe">
-      {post.author.firstName[0] + post.author.lastName[0]}
-    </Avatar>
-  }
-  
-  subheader={`Posted by ${post.author.username}`}
-  sx={{
-    "& .MuiCardHeader-subheader": {
-      fontSize: "1.2rem",
-      fontWeight: "bold",
-    },
-  }}
-/>
+  const handelMenuOpen = (event) => {
+    setAnchorE1(event.currentTarget);
+  };
 
-<CardContent>
-  <Typography
-    variant="body2"
-    color="text.secondary"
-    align="center"
-  >
-    {post.content}
-  </Typography>
-</CardContent>
+  const handelMenuClose = () => {
+    setAnchorE1(null);
+  };
+  return (
+    <Card sx={{ maxWidth: "100%" }}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe">
+            {post.author.firstName[0] + post.author.lastName[0]}
+          </Avatar>
+        }
+        subheader={`Posted by ${post.author.username}`}
+        sx={{
+          "& .MuiCardHeader-subheader": {
+            fontSize: "1.2rem",
+            fontWeight: "bold",
+          },
+        }}
+      />
 
-<CardActions disableSpacing sx={{ mt: "auto" }}>
-  <IconButton aria-label="upvote">
-    <ThumbUp />
-  </IconButton>
-  <IconButton aria-label="comment">
-    <Comment />
-  </IconButton>
-</CardActions>
-</Card>
+      <CardContent>
+        <Typography variant="body2" color="text.secondary" align="center">
+          {post.content}
+        </Typography>
+      </CardContent>
+
+      <CardActions disableSpacing sx={{ mt: "auto" }}>
+        <IconButton aria-label="upvote">
+          <ThumbUp />
+        </IconButton>
+        <IconButton aria-label="comment">
+          <Comment />
+        </IconButton>
+        <IconButton aria-label="more" onClick={handelMenuOpen}>
+          <MoreVert />
+        </IconButton>
+        <Menu
+          anchorEl={anchorE1}
+          open={Boolean(anchorE1)}
+          onClose={handelMenuClose}
+        >
+          <MenuItem onClick={handelMenuClose}> Update</MenuItem>
+          <MenuItem onClick={handelMenuClose}>Delete</MenuItem>
+        </Menu>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default Post;
