20fce0ad7db4688c90536e3ec173046a83ef51dc
Sachinthaka Gayashan <sachinthakagayashan44@gmail.com>
2024-09-08 17:50:16 +0530

    changing home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Container ,Grid} from "@mui/material";
 import axios from "axios";
-import Post from "../components/post";
+import Post from "../components/Post";
 
 function HomePage() {
   const [posts, setPosts] = useState(null);
