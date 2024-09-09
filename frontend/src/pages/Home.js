7af5328d7319b59efe64ef5243d98de85175984a
Pamudu Sarasith <pamudu.lelkada@gmail.com>
2024-09-09 14:11:17 +0530

    Refactor Home.js: Add headings for top communities and latest posts

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import axios from "axios";
 import Post from "../components/post";
 import CommunityCarousel from "../components/CommunityCarousel";
@@ -20,7 +20,9 @@ function HomePage() {
   }, []);
   return (
     <Container maxWidth="md" sx={{ mt: 3, mb: 3 }}>
+      <Typography variant="h5" sx={{ mb: 2 }}> Top Communities </Typography>
       <CommunityCarousel />
+      <Typography variant="h5" sx={{ mt: 4 }}> Latest Posts </Typography>
       <Grid container spacing={7} sx={{ mt: 2 }}>
         {posts?.map((post, index) => (
           <Grid item xs={20} key={index}>
